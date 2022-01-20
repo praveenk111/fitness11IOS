0f9c110cc4c34a819fd7a93a684f920c7dc29b14
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2022-01-20 09:49:55 -0500

    added vmi

---
+++ fitness11IOS/bmi_UI.swift
@@ -0,0 +1,20 @@
+//
+//  bmi_UI.swift
+//  fitness11IOS
+//
+//  Created by jatinder kaur on 2022-01-20.
+//
+
+import SwiftUI
+
+struct bmi_UI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct bmi_UI_Previews: PreviewProvider {
+    static var previews: some View {
+        bmi_UI()
+    }
+}
