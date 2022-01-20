738c7a71b5cb918bcf02f51b8068181c5bb22209
Manpreet636 <75639402+Manpreet636@users.noreply.github.com>
2022-01-20 09:38:02 -0500

    Revert "diet plan added"
    This reverts commit 080439fd0269775020f2f2f33757da5ae87d6a56.

---
+++ fitness11IOS/Preview Content/bmi.swift
@@ -0,0 +1,20 @@
+//
+//  bmi.swift
+//  fitness11IOS
+//
+//  Created by Manpreet Kaur on 2022-01-19.
+//
+
+import SwiftUI
+
+struct bmi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct bmi_Previews: PreviewProvider {
+    static var previews: some View {
+        bmi()
+    }
+}
